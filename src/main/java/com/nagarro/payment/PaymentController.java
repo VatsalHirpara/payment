d2c4adb42a7e17cd632061725deedfb1f19b1b2b
Vatsal Hirpara <vatsalplay9@gmail.com>
2021-02-24 00:12:43 +0530

    Create PaymentController.java

---
+++ src/main/java/com/nagarro/payment/PaymentController.java
@@ -0,0 +1,15 @@
+package com.nagarro.payment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+	@GetMapping("/{customerId}")
+	public boolean getPaymentStatus() {
+		return true;
+	}
+}
