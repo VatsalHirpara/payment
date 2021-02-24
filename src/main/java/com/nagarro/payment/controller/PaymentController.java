7bc123dec392ddca95a4db2d90ff6ef3291289c5
Vatsal Hirpara <vatsalplay9@gmail.com>
2021-02-25 00:18:06 +0530

    add /payments/{customerId}

--- src/main/java/com/nagarro/payment/controller/PaymentController.java
+++ src/main/java/com/nagarro/payment/controller/PaymentController.java
@@ -1,4 +1,4 @@
-package com.nagarro.payment;
+package com.nagarro.payment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
 	@GetMapping("/{customerId}")
-	public boolean getPaymentStatus() {
+	public boolean getPaymentStatus(Long customerId) {
 		return true;
 	}
 }
